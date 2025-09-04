ddd5829143e1fc7fbedc33a7b902ec3d16ec2d0e
FIC <fic@info.local>
2025-09-03 21:51:46 -0300

    git

---
+++ src/main/java/br/com/ecommerce/api/service/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.ecommerce.api.service;
+
+import br.com.ecommerce.api.repository.ClienteRepository;
+import br.com.ecommerce.api.model.Cliente;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    // Injecao de Dependencia
+    // Falar que Service Depende de alguém
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository repo) {
+        this.clienteRepository = repo;
+    }
+
+    public List<Cliente> listarTodos() {
+
+        return clienteRepository.findAll();
+    }
+
+}
